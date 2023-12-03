6f7f9209c8da6db146fc10e264f069f673a754d7
Sean Chen <kamiyo@gmail.com>
2023-12-03 13:50:50 -0600

    3

--- AoC2023/03.fsx
+++ AoC2023/03.fsx
@@ -4,15 +4,15 @@ open System.Text.RegularExpressions
 
 let openFile filename = File.ReadAllLines filename
 
-let symbols = [| "@"; "*"; "/"; "="; "&"; "#"; "+"; "-"; "$"; "%" |]
+let symbols = [| '@'; '*'; '/'; '='; '&'; '#'; '+'; '-'; '$'; '%' |]
 
 let regex = new Regex(@"(\d+)", RegexOptions())
 
 let parseLine totalRows idx (line: string) =
     let lineLength = line.Length
     let result = regex.Matches(line)
-    let indices = result |> Seq.map (fun m -> m.Index)
-    let lengths = result |> Seq.map (fun m -> m.Length)
+    let indices = result |> Seq.map (fun m -> m.Index) |> List.ofSeq
+    let lengths = result |> Seq.map (fun m -> m.Length) |> List.ofSeq
 
     let toCheck =
         Seq.map2
@@ -31,10 +31,37 @@ let parseLine totalRows idx (line: string) =
                 |> List.ofSeq)
             indices
             lengths
-        
-        
-    printfn "%A %A %A %A" result indices lengths (toCheck |> List.ofSeq)
+
+    result |> List.ofSeq |> List.map (fun m -> m.Groups[1].Value |> int), (toCheck |> List.ofSeq)
+// printfn "%A %A %A %A" result indices lengths (toCheck |> List.ofSeq)
+
+let rec checkSurrounding (grid: string array) (cells: (int * int) list) (accum: (int * int) list) =
+    match cells with
+    | (x, y) :: t ->
+        match grid[y].ToCharArray()[x] with
+        | '*' -> checkSurrounding grid t ((x, y) :: accum)
+        | _ -> checkSurrounding grid t accum
+    | [] -> accum
 
 let input = "./inputs/03.txt" |> openFile
 let rows = input.Length
-input |> Array.iteri (parseLine rows)
+
+input
+|> Array.mapi (parseLine rows)
+|> Array.map (fun (nums, coords) -> List.map2 (fun n c -> checkSurrounding input c [], n) nums coords)
+|> Array.reduce List.append
+|> List.fold (fun state (cs, n) -> cs |> List.map (fun c -> c, n) |> List.append state) ([]: ((int * int) * int) list)
+|> List.fold
+    (fun (map: Map<int * int, int list>) (c, n) ->
+        match map.TryFind c with
+        | None -> map.Add(c, [ n ])
+        | Some(l) -> map.Add(c, n :: l))
+    Map.empty
+// |> List.filter (fun l -> l |> fst |> List.isEmpty |> not)
+// |> List.map Option.get
+// |> List.reduce (fun l r -> l + r)
+|> Map.toList
+|> List.filter (fun (_, ns) -> ns.Length = 2)
+|> List.map (fun (_, ns) -> ns |> List.reduce (fun l r -> l * r))
+|> List.reduce (fun l r -> l + r)
+|> printfn "%A"
