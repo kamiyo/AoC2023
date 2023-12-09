91f6dd13d5e764f2162c1f44ef4ed2aadb0efd98
Sean Chen <kamiyo@gmail.com>
2023-12-08 23:24:50 -0600

    9

--- AoC2023/08.fsx
+++ AoC2023/08.fsx
@@ -46,12 +46,57 @@ let rec traverseGhost
 
         traverseGhost mapping instructions newPos ((currentInstr + 1) % instructions.Length) (steps + 1u)
 
-let rec gcd (a: uint) (b: uint) =
+let detectCycleBrent (mapping: PositionMap) (instructions: string) (initial: string) =
+    let mutable power = 1L
+    let mutable lambda = 1L
+    let mutable hareInstr = 0
+    let mutable tortoise = initial, 0
+    
+    let next ((currentPos, instrIdx): string * int) =
+        let currMap = mapping[currentPos]        
+        match instructions[instrIdx] with
+        | 'L' -> currMap |> fst, (instrIdx + 1) % instructions.Length
+        | 'R' -> currMap |> snd, (instrIdx + 1) % instructions.Length
+        | _ -> raise (Exception "Shouldn't be here")
+    
+    let mutable hare = initial, 0
+    hare <- next hare
+        
+    while tortoise <> hare do
+        if power = lambda then
+            tortoise <- hare
+            power <- power * 2L
+            lambda <- 0
+        hare <- next hare
+        lambda <- lambda + 1L
+        
+    tortoise <- initial, 0
+    hare <- initial, 0
+    hareInstr <- 0
+    for _ in 0L .. lambda - 1L do
+        hare <- next hare
+    
+    let mutable mu = 0L
+    while tortoise <> hare do
+        tortoise <- next tortoise
+        hare <- next hare
+        mu <- mu + 1L
+
+    initial, lambda, mu
+
+let iterMap (mapping: PositionMap) (instructions: string) ((currentPos, instrIdx): string * int) =
+    let currMap = mapping[currentPos]        
+    match instructions[instrIdx] with
+    | 'L' -> currMap |> fst, (instrIdx + 1) % instructions.Length
+    | 'R' -> currMap |> snd, (instrIdx + 1) % instructions.Length
+    | _ -> raise (Exception "Shouldn't be here")
+
+let rec gcd (a: uint64) (b: uint64) =
     match b with
-    | 0u -> a
+    | 0UL -> a
     | _ -> gcd b (a % b)
 
-let lcm (a: uint) (b: uint) =
+let lcm (a: uint64) (b: uint64) =
     a * b / (gcd a b)
 
 let input = readFile "./inputs/08.txt"
@@ -67,10 +112,42 @@ let mapping =
             state |> Map.add k v)
         (Map<string, string * string>([]))
 
-mapping
-|> Map.filter (fun k v -> k.Chars 2 = 'A')
-|> Map.keys
-|> List.ofSeq
-|> List.map (fun k -> traverseGhost mapping instructions k 0 0u)
-|> List.reduce (fun l r -> l * r)
-|> printfn "%A"
+let starts =
+    mapping
+    |> Map.filter (fun k v -> k.Chars 2 = 'A')
+    |> Map.keys
+    |> List.ofSeq
+let cycles = 
+    starts
+    // |> List.map (fun k -> traverseGhost mapping instructions k 0 0u)
+    |> List.map (fun k -> detectCycleBrent mapping instructions k)
+    // |> List.reduce (fun l r -> l * r)
+    |> printfn "%A"
+
+let rec findEndPos (mapping: PositionMap) (instructions: string) ((currentPos, instrIdx): string * int) (count: int) =
+    match currentPos.Chars 2 = 'Z' with
+    | true -> count
+    | false -> findEndPos mapping instructions (iterMap mapping instructions (currentPos, instrIdx)) (count + 1)
+
+let endPos =
+    starts
+    |> List.map (fun s -> findEndPos mapping instructions (s, 0) 0)
+    |> List.map uint64
+    |> List.reduce lcm
+printfn "%A" endPos
+
+// let test =
+//     starts
+//     |> List.map (fun s ->
+//         let mutable st = (s, 0)
+//         for _ in 0 .. 1977812634 do
+//             st <- iterMap mapping instructions st
+//         st)
+// let mutable aaa = ("AAA", 0)
+// for _ in 0 .. 1 do
+//     aaa <- iterMap mapping instructions aaa
+// for _ in 0 .. 1977812635 do
+//     aaa <- iterMap mapping instructions aaa
+// printfn "%A" test
+
+// 2 + (x * 18113) + 18111
---
+++ AoC2023/09.fsx
@@ -0,0 +1,29 @@
+﻿open System
+open System.IO
+
+let readFile input = File.ReadAllLines input
+
+let rec getNextInSequence (history: int list) =
+    match history |> List.distinct |> List.length = 1 with
+    | true -> history |> List.last
+    | false ->
+        let diffs =
+            List.map2 (fun l r -> l - r) (history |> List.tail) (history |> List.take ((history |> List.length) - 1))
+
+        (history |> List.last) + (getNextInSequence diffs)
+        
+let rec getPrevInSequence (history: int list) =
+    match history |> List.distinct |> List.length = 1 with
+    | true -> history |> List.head
+    | false ->
+        let diffs =
+            List.map2 (fun l r -> l - r) (history |> List.tail) (history |> List.take ((history |> List.length) - 1))
+        let next = getPrevInSequence diffs
+        (history |> List.head) - next
+
+"./inputs/09.txt"
+|> readFile
+|> Array.map (fun l -> l.Split(" ") |> Array.map int |> List.ofArray)
+|> Array.map getPrevInSequence
+|> Array.reduce (fun l r -> l + r)
+|> printfn "%A"
